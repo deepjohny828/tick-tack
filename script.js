830472254781ad16fba1a3b5b0982c830dc6807d
Johny <deepjohny828@gmail.com>
2024-10-10 10:30:01 +0300

    complete with some problems

--- script.js
+++ script.js
@@ -92,10 +92,16 @@ const GameBoard = (function () {
             return false;
         }
     };
+    const clearGameBoard = () => gameBoard = [
+        [, ,],
+        [, ,],
+        [, ,],
+    ];
     const print = () => {
         console.table(gameBoard);
     };
     return {
+        clearGameBoard,
         whoWinner,
         print,
         setBoardCell,
@@ -109,88 +115,130 @@ function player(name) {
     const addPoint = () => point++;
     const rejectPoint = () => point--;
     const getPoint = () => point;
+    const resetPoint = () => point = 0
     return {
         name,
+        resetPoint,
         addPoint,
         rejectPoint,
         getPoint,
     };
 }
-
+function clearDOMBoard(){
+    const btn = document.querySelectorAll(".cell")
+    btn.forEach((item) => item.textContent = "")
+}
 const init = (function () {
-    const player1 = player("joe");
-    const player2 = player("joseph");
+    
+    const containerCell = document.querySelector(".container");
+    const startBtn = document.querySelector("#start-btn")
+    const namePlayer1 = (document.querySelector("#name-player1").value) === "" ? "Anonim 1" : document.querySelector("#name-player1").value
+    const namePlayer2 = (document.querySelector("#name-player2").value) === "" ? "Anonim 2" : document.querySelector("#name-player2").value
+    const scorePlayer1 = document.querySelector("#score-player1")
+    const scorePlayer2 = document.querySelector("#score-player2")
+    const statusRound = document.querySelector("#status-round")
+    const statusGame = document.querySelector("#status-game")
+    const player1 = player(namePlayer1);
+    const player2 = player(namePlayer2);
+
+    let stopGame = false;
     let count = 0;
     let symbol;
-    const containerCell = document.querySelector(".container");
+
+    startBtn.addEventListener("click",() => {
+        if(player1.getPoint() === 3 || player2.getPoint() === 3){
+            player1.resetPoint()
+            player2.resetPoint()
+            scorePlayer1.textContent = player1.getPoint()
+            scorePlayer2.textContent = player2.getPoint()
+            statusGame.textContent = ""
+        }
+        clearDOMBoard();
+        GameBoard.clearGameBoard();
+        stopGame = false
+        statusRound.textContent = ""
+        symbol = "O"
+    })
     containerCell.addEventListener("click", (e) => {
-        switch (e.target.id) {
-            case "1":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(0, 0, symbol);
-                e.target.textContent = GameBoard.getBoardCell(0, 0);
-                break;
-            case "2":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(0, 1, symbol);
-                e.target.textContent = GameBoard.getBoardCell(0, 1);
-                break;
-            case "3":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(0, 2, symbol);
-                e.target.textContent = GameBoard.getBoardCell(0, 2);
-                break;
-            case "4":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(1, 0, symbol);
-                e.target.textContent = GameBoard.getBoardCell(1, 0);
-                break;
-            case "5":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(1, 1, symbol);
-                e.target.textContent = GameBoard.getBoardCell(1, 1);
-                break;
-            case "6":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(1, 2, symbol);
-                e.target.textContent = GameBoard.getBoardCell(1, 2);
-                break;
-            case "7":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(2, 0, symbol);
-                e.target.textContent = GameBoard.getBoardCell(2, 0);
-                break;
-            case "8":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(2, 1, symbol);
-                e.target.textContent = GameBoard.getBoardCell(2, 1);
-                break;
-            case "9":
-                symbol === "X" ? (symbol = "O") : (symbol = "X");
-                GameBoard.setBoardCell(2, 2, symbol);
-                e.target.textContent = GameBoard.getBoardCell(2, 2);
-                break;
-        }
-        if (count > 3) {
-            if (GameBoard.whoWinner()) {
-                console.log("Player 1 Win");
-                player1.addPoint();
-            } else if (GameBoard.whoWinner() != null) {
-                console.log("Player 2 Win");
-                console.log({
-                    GameBoard,
-                });
-                player2.addPoint();
-            } else if (count > 8) {
-                console.log("Draw");
+        if(stopGame === false && player1.getPoint() < 3 && player2.getPoint() < 3){
+            switch (e.target.id) {
+                case "1":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(0, 0, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(0, 0);
+                    break;
+                case "2":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(0, 1, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(0, 1);
+                    break;
+                case "3":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(0, 2, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(0, 2);
+                    break;
+                case "4":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(1, 0, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(1, 0);
+                    break;
+                case "5":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(1, 1, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(1, 1);
+                    break;
+                case "6":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(1, 2, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(1, 2);
+                    break;
+                case "7":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(2, 0, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(2, 0);
+                    break;
+                case "8":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(2, 1, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(2, 1);
+                    break;
+                case "9":
+                    symbol === "X" ? (symbol = "O") : (symbol = "X");
+                    GameBoard.setBoardCell(2, 2, symbol);
+                    e.target.textContent = GameBoard.getBoardCell(2, 2);
+                    break;
             }
+            if (count > 3) {
+                if (GameBoard.whoWinner()) {
+                    console.log("Player 1 Win");
+                    if(player1.getPoint() === 2){
+                        statusGame.textContent = "Player 1 Win"
+                    }
+                    statusRound.textContent = "Player 1 Win"
+                    stopGame = true
+                    count = -1
+                    player1.addPoint();
+                    scorePlayer1.textContent = player1.getPoint()
+                } else if (GameBoard.whoWinner() != null) {
+                    console.log("Player 2 Win");
+                    if(player2.getPoint() === 2){
+                        statusGame.textContent = "Player 2 Win"
+                    }
+                    statusRound.textContent = "Player 2 Win"
+                    stopGame = true
+                    count = -1
+                    player2.addPoint();
+                    scorePlayer2.textContent = player2.getPoint()
+                } else if (count > 8) {
+                    console.log("Draw");
+                    statusRound.textContent = "Draw"
+                    count = -1
+                    stopGame = true
+                }
+            }
+            count++;
+            GameBoard.print();
         }
-        count++;
-        GameBoard.print();
+        
     });
-    
-    // console.log({
-    //     point1: player1.getPoint(),
-    //     point2: player2.getPoint(),
-    // });
 })();
